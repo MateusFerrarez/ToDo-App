0bfda19034b788a1c304d815209edf7ab29f5fcc
Mateus Ferrarez <mateusferrarez16@gmail.com>
2025-02-28 18:55:18 -0300

    First commit

---
+++ todoApp/src/main/java/br/mateus/todoapp/TodoApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.mateus.todoapp;
+
+/**
+ *
+ * @author mateu
+ */
+public class TodoApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
